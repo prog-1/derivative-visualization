ca1bdb27ae618c00e2288194a19d27097233a177
Alex <stukalovsaleksandrs@gmail.com>
2023-11-19 16:55:40 +0200

    Added tangent visualization for X>0

--- main.go
+++ main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"fmt"
 	"image"
 	"image/color"
 	"log"
 	"math"
 
 	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
 	"gonum.org/v1/plot"
 	"gonum.org/v1/plot/plotter"
 	"gonum.org/v1/plot/vg/draw"
@@ -16,39 +18,55 @@ import (
 const (
 	screenWidth  = 960
 	screenHeight = 600
+	eps          = 0.000001
 )
 
 type game struct {
-	p *plot.Plot
+	f       func(float64) float64
+	NewPlot func() *plot.Plot
+	p       *plot.Plot
 }
 
 func NewGame() *game {
-	p := plot.New()
-	p.X.Min = -10
-	p.X.Max = 10
-	p.Y.Min = -10
-	p.Y.Max = 10
-
-	graph := plotter.NewFunction(func(x float64) float64 { return math.Sin(x) })
-	graph.Color = color.RGBA{0, 0, 255, 255}
-	p.Add(graph)
 
 	return &game{
-		p,
+		func(x float64) float64 { return math.Tan(x) },
+		func() *plot.Plot {
+			p := plot.New()
+			p.X.Min = 0
+			p.X.Max = 10
+			p.Y.Min = -10
+			p.Y.Max = 10
+
+			p.BackgroundColor = color.Black
+			return p
+		},
+		nil,
 	}
 }
 
 func (g *game) Layout(outWidth, outHeight int) (w, h int) { return screenWidth, screenHeight }
 func (g *game) Update() error {
+	g.p = g.NewPlot()
+
+	f := plotter.NewFunction(g.f)
+	f.Color = color.RGBA{255, 255, 255, 255}
+	g.p.Add(f)
+
+	t := plotter.NewFunction(GetTangent(g.f, GetCursorX(g.p)))
+	t.Color = color.RGBA{255, 255, 255, 255}
+	g.p.Add(t)
+
 	return nil
 }
 func (g *game) Draw(screen *ebiten.Image) {
 	DrawPlot(screen, g.p)
+	ebitenutil.DebugPrint(screen, fmt.Sprintf("X: %v", GetCursorX(g.p)))
 }
 
 func main() {
 	ebiten.SetWindowSize(screenWidth, screenHeight)
-	ebiten.SetWindowTitle("Derivative plotter")
+	ebiten.SetWindowTitle("Tangent plotter")
 	g := NewGame()
 	if err := ebiten.RunGame(g); err != nil {
 		log.Fatal(err)
@@ -62,4 +80,27 @@ func DrawPlot(screen *ebiten.Image, p *plot.Plot) {
 	p.Draw(draw.New(c))
 
 	screen.DrawImage(ebiten.NewImageFromImage(c.Image()), &ebiten.DrawImageOptions{})
+}
+
+func GetCursorX(p *plot.Plot) float64 {
+	tmpx, _ := ebiten.CursorPosition()
+	x := float64(tmpx)
+	return x / (screenWidth / (p.X.Max - p.X.Min))
+}
+
+func GetTangent(f func(float64) float64, x0 float64) func(float64) float64 {
+	// TODO:
+	// 1. Get cursor's X coordinate +
+	// 2. Draw an according tangent
+	// 2.1. Calculate f(x) +
+	// 2.2. Calculate f'(x)
+	// 2.3. Calculate b
+	// 2.5. Draw the line
+	x1 := x0 + eps
+	y0, y1 := f(x0), f(x1)
+	k := (y1 - y0) / (x1 - x0)
+	b := y0 - k*x0
+	return func(x float64) float64 {
+		return k*x + b
+	}
 }
